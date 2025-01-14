34e1847540da29999a96a1b88e7a052fbc439835
sld-web <sadeep.web@yahoo.com>
2025-01-14 18:48:01 +0530

    basic structure

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Select all the buttons that should trigger an action
 const buttons = document.querySelectorAll('.one, .two, .three, .four, .five, .six, .seven, .eight, .nine, .zero, .dot, .sum, .plus, .minus, .multiply, .division, .percen');
-
+let user = '';
+let user2 = '';
+let first = 0;
+let operator = '';
+let second = 0;
 // Add event listener to all buttons
 buttons.forEach(button => {
     button.addEventListener('click', () => {
@@ -8,6 +12,23 @@ buttons.forEach(button => {
         const div = document.createElement('div');
         div.innerText = button.innerText;
         display.appendChild(div);
+        if (div.innerText !== '='){
+        if (div.innerText >= '0' && div.innerText <= '9' || div.innerText === '.' ) {
+            if (operator) {
+                user2 += div.innerText; 
+                second = Number(user2);
+            } else {
+                user += div.innerText; 
+                first = Number(user);
+            }
+        } else{
+            operator = div.innerText;
+        }}
+        console.log(first);
+        console.log(operator);
+        console.log(second);
+        
+        
     });
 });
 
@@ -24,7 +45,67 @@ reset.addEventListener('click', function() {
     while (display.firstChild) {
         display.removeChild(display.firstChild);  
     }
+    user = '';
+    user2 = '';
+    first = 0;
+    second = 0;
+    result = 0;
 });
+
+function add (a,b){
+    return a+b;
+}
+
+function substraction (a,b){
+    return a-b;
+}
+
+function multiply (a,b){
+    return a*b;
+}
+
+function division (a,b){
+    return a/b;
+}
+
+function percentage (a,b){
+    return a*b/100;
+}
+const equals = document.querySelector('.sum'); // Make sure you have a button with class 'equals'
+equals.addEventListener('click', function() {
+    let result;
+     switch (operator){
+        case '+':
+            result = add(first,second);
+            break;
+        case '-':
+            result = substraction(first,second);
+            break;
+        case 'x':
+            result = multiply(first,second);
+            break;
+        case '/':
+            result = division(first,second);
+            break;
+        case '%':
+            result = percentage(first,second);
+            break;
+        default:
+            return 'error';
+}
+    while (display.firstChild) {
+        display.removeChild(display.firstChild);  
+    }
+    const div = document.createElement('div');
+    div.innerText = result;
+    display.appendChild(div);
+    first = result;
+    second = 0;
+    user2 = '';
+console.log(result);
+});
+
+
 
 
 
